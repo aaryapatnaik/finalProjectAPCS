4bff9139190fafd3de68f937ad555b0ad52a95a9
hariniinarayanan <harinibnarayanan@gmail.com>
2022-05-10 09:57:10 -0700

    board class

---
+++ PacManGame/src/Board.java
@@ -0,0 +1,25 @@
+public class Board {
+    private final int width = 28;
+    private final int height = 28;
+    Cell[][] gameBoard = new Cell[height][width];
+
+    public Board() {
+        for (int i = 0; i < gameBoard.length; i++) {
+            for (int k = 0; k < gameBoard[i].length; k++) {
+                gameBoard[i][k] = new Cell(false, true);
+            }
+        }
+        for (int i = 0; i < gameBoard[0].length; i++) {
+            gameBoard[0][i] = new Cell(true,false);
+        }
+        for (int i = 0; i < gameBoard[0].length; i++) {
+            gameBoard[height-1][i] = new Cell(true,false);
+        }
+        for (int i = 0; i < gameBoard.length; i++) {
+            gameBoard[i][0] = new Cell(true,false);
+        }
+        for (int i = 0; i < gameBoard.length; i++) {
+            gameBoard[i][width-1] = new Cell(true,false);
+        }
+    }
+}
