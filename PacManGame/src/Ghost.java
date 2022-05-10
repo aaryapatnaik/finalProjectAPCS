817b47eb38caf916ff18696475c648c904c98b5c
jindo85547 <imjustin679@gmail.com>
2022-05-10 09:49:48 -0700

    added vulnerability methods for ghost

--- PacManGame/src/Ghost.java
+++ PacManGame/src/Ghost.java
@@ -3,9 +3,20 @@ import java.awt.*;
 
 public class Ghost extends Moveable {
 
+    private static boolean isVulnerable;
+
     //constructor for the normal ghost
     public Ghost(String na, Image[] imgsIdle, Image[] imgsActive, int dx, int dy, int spawnX, int spawnY) {
         super(na, imgsIdle, imgsActive, dx, dy, spawnX, spawnY);
+        isVulnerable = false;
 	}
+
+    public boolean getVulnerable() {
+        return isVulnerable;
+    }
+
+    public void setVulnerable(boolean ent) {
+        isVulnerable = ent;
+    }
 
 }
