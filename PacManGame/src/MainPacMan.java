ba9ea19770a9e96f07e73a89dc0140748131ab7c
Aarya Patnaik <aaryapatnaik@gmail.com>
2022-05-10 18:22:13 -0700

    put display class stuff into main pac man class because it couldn't compile

--- PacManGame/src/MainPacMan.java
+++ PacManGame/src/MainPacMan.java
@@ -1,18 +1,31 @@
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import javax.swing.JFrame;
 import javax.swing.ImageIcon;
-public class MainPacMan {
+
+public class MainPacMan{
     private JFrame frame;
     private ImageIcon lasagna;
     private JLabel myLabel;
-
+    private JPanel panel;
     public MainPacMan() {
-
-        lasagna = new ImageIcon("/Users/aarya/Desktop/AP_CompSci_FinalProject/finalProjectAPCS/PacManGame/src/lasagna.png");
+        //Lasagna
+        BufferedImage img = null;
+        try {
+            img = ImageIO.read(new File("/Users/aarya/Desktop/AP_CompSci_FinalProject/finalProjectAPCS/PacManGame/src/lasagna.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Image dimg = img.getScaledInstance(600, 400,Image.SCALE_SMOOTH);
+        ImageIcon lasagna = new ImageIcon(dimg);
         myLabel = new JLabel(lasagna);
-        myLabel.setSize(400,600);
-
+        
+        //Actual frame
         JFrame frame = new JFrame("PacMan Game by AP, AV, JI, HN");
         frame.add(myLabel);
         frame.setSize(800, 800);
@@ -20,21 +33,10 @@ public class MainPacMan {
         frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
-
-        private Image getScaledImage(Image srcImg, int w, int h){
-           BufferedImage resizedImg = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
-          Graphics2D g2 = resizedImg.createGraphics();
-    
-          g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
-          g2.drawImage(srcImg, 0, 0, w, h, null);
-          g2.dispose();
-    
-        return resizedImg;
-    }
     }
     public static void main(String args[])
     {
-        new Display();
->>>>>>> befce51716926f859346073dbc0affe032d49ce6
+        new MainPacMan();
     }
+
 }
