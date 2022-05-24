912b2b32143e6b7d9a349d92e9a111e3c438ff48
hariniinarayanan <harinibnarayanan@gmail.com>
2022-05-24 10:23:30 -0700

    edited draw method again

--- PacManGame/src/Board.java
+++ PacManGame/src/Board.java
@@ -371,8 +371,12 @@ public class Board {
         for (int i = 0; i < gameBoard.length; i++) {
             for (int j = 0; j < gameBoard[i].length; i++) {
                 if ((gameBoard[i][j]).getContainsFood()) {
-                    ImageIcon img = new ImageIcon("/Users/harininarayanan/apcs/finalProjectAPCS/Media/lasagna.png");
-                    img.paintIcon(c, g, j*30, i*30);
+                    ImageIcon img = new ImageIcon("// small lasagna pathway //");
+                    img.paintIcon(c, g, j*25, i*25);
+                }
+                if ((gameBoard[i][j]).getContainsBigFood()) {
+                    ImageIcon img = new ImageIcon("// big lasagna pathway //");
+                    img.paintIcon(c, g, j*25, i*25);
                 }
             }
         }
