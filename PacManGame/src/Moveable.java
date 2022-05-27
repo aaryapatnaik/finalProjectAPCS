c8574214cddf454b2e77b216159fd81c407a01d4
jindo85547 <imjustin679@gmail.com>
2022-05-27 10:32:48 -0700

    updated direction conventions in checkSurroundings and the turning methods

--- PacManGame/src/Moveable.java
+++ PacManGame/src/Moveable.java
@@ -12,9 +12,9 @@ public class Moveable {
 	/** rect has info about location and dimension of this game object*/
 	private Rectangle rect;
 
-	private String name; // will be used in main for distinction between ghosts
+	//private String name; will be used in main for distinction between ghosts
 	
-	private Image[] images;// for animating a character. The player should have 16 images, the ghost should have 32 images.
+	//private Image[] images; for animating a character. The player should have 16 images, the ghost should have 32 images.
 	//images 0-7 should be moving images for the player. 8-16 should be idle images, 2 in each direction. The ghost will
 	//have a set of 8 images for moving normally, idle normal, and moving blue and idle blue. 
 
@@ -96,9 +96,9 @@ public class Moveable {
 		return rect;
 	}
 
-	public Image[] getImageArray() {
+	/*public Image[] getImageArray() {
         return this.images;
-    }
+    }*/
   
 	//should return true or false when the player collides with a ghost
 	public boolean collidedWithMoveable(Moveable go) {
@@ -114,29 +114,28 @@ public class Moveable {
 	/*checks if there is a cell wall either above, below, to the left, to the right. Returns an arraylist with numbers
 	that represent possible directions for both the player and the ghost. This can be used to randomly sort through direction
 	choices for ghost and present possible directions for player when it hits a wall.
-	0 is north, 1 is east, 2 is south, and 3 is west*/
+	0 is right, 1 is left , 2 is up, and 3 is down*/
 	// this method should probably be in the main pac man class. 
 	public ArrayList<Integer> checkSurroundings(Moveable m, Cell[][] b) {
 		int x = (int)(m.getX()); //divide by cell width
 		int y = (int)(m.getY()); //divide by cell width
 
-		ArrayList<Integer> dir = new ArrayList();
+		ArrayList<Integer> dir = new ArrayList<Integer>();
 
 		//check for null, then check if the cell is a solid wall
 		//gameboard is a temporary name, it should be whatever the board name in main is
 		if (b[y+1][x] != null && b[y+1][x].getWall() == true) {
-			dir.add(0);
-		}
-		if (b[y-1][x] != null && b[y-1][x].getWall() == true) {
-			dir.add(1);
-		}
-		if (b[y][x+1] != null && b[y][x+1].getWall() == true) {
 			dir.add(2);
 		}
-		if (b[y][x-1] != null && b[y][x-1].getWall() == true) {
+		if (b[y-1][x] != null && b[y-1][x].getWall() == true) {
 			dir.add(3);
 		}
-
+		if (b[y][x+1] != null && b[y][x+1].getWall() == true) {
+			dir.add(0);
+		}
+		if (b[y][x-1] != null && b[y][x-1].getWall() == true) {
+			dir.add(1);
+		}
 		return dir;
 	}
 
@@ -154,11 +153,11 @@ public class Moveable {
 		return "Turned left!";
     }
     public String turnDown() {
-		this.direction = 2;
+		this.direction = 3;
 		return "Turned down!";
     }
     public String turnUp() {
-		this.direction = 3;
+		this.direction = 2;
 		return "Turned up!";
     }
 
